59a8c0c938f3a08b5793fc78ca3a8cf3395b1bbb
Rick Peterman <rick.peterman@prx.org>
2024-09-04 19:46:00 -0500

    feat(datetime): ensure iso 8601 date string have time and offset

--- components/DateTime/DateTime.tsx
+++ components/DateTime/DateTime.tsx
@@ -3,7 +3,7 @@
  * Component for rendering dates.
  */
 
-import { Maybe, RootState } from '@interfaces';
+import type { Maybe, RootState } from '@interfaces';
 import { getSettingsTimeZone } from '@store/reducers';
 import { useStore } from 'react-redux';
 
@@ -26,10 +26,33 @@ export const DateTime = ({
 
   if (!date) return null;
 
-  const usedDateValue =
-    typeof date === 'string' && /^\d{4}-\d{2}-\d{2}$/.test(date)
-      ? `${date}T00:00:00`
-      : date;
+  let usedDateValue = date;
+
+  // Date strings from the API should be in ISO 8601, but could be missing some elements.
+  if (
+    typeof usedDateValue === 'string' &&
+    /^\d{4}-\d{2}-\d{2}/.test(usedDateValue)
+  ) {
+    // Ensure ISO 8601 dates have a time.
+    if (!/T\d{2}:\d{2}:\d{2}/.test(usedDateValue)) {
+      usedDateValue = `${usedDateValue}T00:00:00`;
+    }
+
+    // Ensure ISO 8601 dates are set to settings time-zone if they do not already have a time-zone offset.
+    if (!/[+-]\d{2}:?\d{2}$/.test(usedDateValue)) {
+      // Time-zone from settings will be in long format, eg `America/New_York`.
+      // We need to convert that to a offset string.
+      const tz = new Date()
+        .toLocaleTimeString('en-US', {
+          ...(timeZone && { timeZone }),
+          timeZoneName: 'longOffset'
+        })
+        .match(/GMT(.+)/)?.[1];
+
+      usedDateValue = `${usedDateValue}${tz}`;
+    }
+  }
+
   const renderDate =
     usedDateValue instanceof Date ? usedDateValue : new Date(usedDateValue);
   const formattedDate = renderDate.toLocaleDateString(locale || 'en-US', {
@@ -38,7 +61,7 @@ export const DateTime = ({
   });
 
   return (
-    <time className={className} dateTime={`${usedDateValue}`}>
+    <time className={className} dateTime={`${renderDate.toISOString()}`}>
       {formattedDate}
     </time>
   );
