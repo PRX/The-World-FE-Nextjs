24c0ec20e67c5fc54e5d9e7760b2ca29430baad8
Rick Peterman <rick.peterman@prx.org>
2024-10-07 14:11:37 -0500

    feat(appheadernav): sticky donate link btn

--- components/AppHeader/AppHeaderNav/AppHeaderNav.styles.ts
+++ components/AppHeader/AppHeaderNav/AppHeaderNav.styles.ts
@@ -3,6 +3,8 @@
  * Styles for AppHeaderNav.
  */
 
+import { keyframes } from 'tss-react';
+import { makeStyles } from 'tss-react/mui';
 import { alpha, createTheme, Theme } from '@mui/material/styles';
 
 export const appHeaderNavTheme = (theme: Theme) =>
@@ -13,7 +15,7 @@ export const appHeaderNavTheme = (theme: Theme) =>
           text: {
             color: theme.palette.primary.contrastText,
             fontWeight: theme.typography.fontWeightBold,
-            '&:hover, &:focus': {
+            '&:hover, &:focus-visible': {
               backgroundColor: alpha(theme.palette.primary.contrastText, 0.1)
             }
           }
@@ -21,3 +23,69 @@ export const appHeaderNavTheme = (theme: Theme) =>
       }
     }
   });
+
+export const appHeaderNavStyles = makeStyles()((theme) => {
+  const stickyOffsetY = theme.spacing(2);
+  const stickyOffsetX = theme.spacing(2);
+  const headerNavStickyConstantProps = `
+      --sticky-box-shadow: ${theme.shadows[4]};
+      --sticky-icon-button-scale: 2;
+      position: fixed;
+      top: 0;
+      right: 0;
+      padding-inline: ${stickyOffsetX};
+      padding-block: ${stickyOffsetY};
+      z-index: ${theme.zIndex.fab};
+  `;
+  const headerNavSticky = keyframes`
+    1% {
+      ${headerNavStickyConstantProps}
+      --sticky-bg-opacity: 0;
+      translate: 0 calc((300% + ${stickyOffsetY} * 2) * -1);
+    }
+
+    100% {
+      ${headerNavStickyConstantProps}
+      --sticky-bg-opacity: 1;
+      translate: 0 0;
+    }
+`;
+
+  return {
+    root: {
+      '@property --sticky-bg-opacity': {
+        syntax: '"<number>"',
+        inherits: 'true',
+        initialValue: 0
+      }
+    },
+    sticky: {
+      '--sticky-bg-opacity': 0,
+      '--sticky-box-shadow': 'none',
+      '--sticky-icon-button-scale': 1,
+      isolation: 'isolate',
+      animation: `${headerNavSticky} ease both`,
+      'animation-timeline': 'view()',
+      'animation-range-start': 'exit 100px',
+      'animation-range-end': 'exit 400px',
+      '& > :not(.MuiIconButton-root)': {
+        boxShadow: 'var(--sticky-box-shadow)'
+      },
+      '& > .MuiIconButton-root': {
+        scale: 'var(--sticky-icon-button-scale)'
+      },
+      '&::before': {
+        pointerEvents: 'none',
+        content: "''",
+        opacity: 'var(--sticky-bg-opacity)',
+        position: 'absolute',
+        inset: '0 0 -200% -200%',
+        zIndex: -1,
+        backgroundImage: `radial-gradient(farthest-side at top right, ${alpha(
+          theme.palette.background.paper,
+          0.6
+        )} 30%, transparent)`
+      }
+    }
+  };
+});
--- components/AppHeader/AppHeaderNav/AppHeaderNav.tsx
+++ components/AppHeader/AppHeaderNav/AppHeaderNav.tsx
@@ -9,7 +9,6 @@ import type {
   IconButtonColors,
   RootState
 } from '@interfaces';
-import { Fragment } from 'react';
 import { useStore } from 'react-redux';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
@@ -18,7 +17,7 @@ import { FavoriteSharp } from '@mui/icons-material';
 import { isLocalUrl } from '@lib/parse/url';
 import { handleButtonClick } from '@lib/routing';
 import { getAppDataMenu } from '@store/reducers';
-import { appHeaderNavTheme } from './AppHeaderNav.styles';
+import { appHeaderNavTheme, appHeaderNavStyles } from './AppHeaderNav.styles';
 
 const iconComponentMap = new Map();
 iconComponentMap.set('heart', FavoriteSharp);
@@ -32,92 +31,103 @@ export const AppHeaderNav = () => {
   const store = useStore<RootState>();
   const state = store.getState();
   const headerNav = getAppDataMenu(state, 'headerNav');
+  const { classes, cx } = appHeaderNavStyles();
+  const menuItems = headerNav
+    ?.filter((v): v is IButtonWithUrl => !!v.url)
+    .map(({ url, ...other }) => ({
+      ...other,
+      url: new URL(url, 'https://theworld.org')
+    }))
+    .map(({ service, ...other }) => {
+      if (!service) return other;
 
-  return headerNav?.length ? (
+      const servicesOptions = new Map([
+        [
+          'prx:give',
+          {
+            icon: 'heart',
+            color: 'secondary',
+            sticky: true
+          }
+        ]
+      ]);
+      const options = servicesOptions.get(service);
+
+      return {
+        ...other,
+        ...options,
+        service
+      };
+    });
+
+  const renderMenuItem = ({
+    key,
+    color,
+    icon,
+    name,
+    url,
+    sticky,
+    attributes,
+    itemLinkClass = ''
+  }) => (
+    <span className={cx({ [classes.sticky]: sticky })} key={key}>
+      <Button
+        sx={{ display: { xs: 'none', sm: 'inline-flex' } }}
+        component="a"
+        href={isLocalUrl(url.href) ? url.pathname || '/' : url.href}
+        onClick={handleButtonClick(url)}
+        variant={
+          /\bbtn-(text|link)\b/.test(itemLinkClass) ? 'text' : 'contained'
+        }
+        color={(color as ButtonColors) || 'primary'}
+        disableRipple
+        disableElevation
+        {...(icon && { startIcon: renderIcon(icon) })}
+        {...attributes}
+      >
+        {name}
+      </Button>
+      {icon ? (
+        <IconButton
+          sx={{ display: { sm: 'none', xs: 'inline-flex' } }}
+          component="a"
+          href={url.href}
+          onClick={handleButtonClick(url)}
+          aria-label={name}
+          color={(color as IconButtonColors) || 'default'}
+          size="small"
+          disableRipple
+          {...attributes}
+        >
+          {renderIcon(icon)}
+        </IconButton>
+      ) : (
+        <Button
+          sx={{ display: { sm: 'none', xs: 'inline-flex' } }}
+          component="a"
+          href={url.href}
+          onClick={handleButtonClick(url)}
+          variant="text"
+          color={(color as ButtonColors) || 'primary'}
+          size="small"
+          disableRipple
+          disableElevation
+          {...attributes}
+          {...(icon && {
+            startIcon: renderIcon(icon)
+          })}
+        >
+          {name}
+        </Button>
+      )}
+    </span>
+  );
+
+  if (!menuItems?.length) return null;
+
+  return (
     <ThemeProvider theme={appHeaderNavTheme}>
-      {headerNav
-        .filter((v): v is IButtonWithUrl => !!v.url)
-        .map(({ url, ...other }) => ({
-          ...other,
-          url: new URL(url, 'https://theworld.org')
-        }))
-        .map(({ service, ...other }) => {
-          if (!service) return other;
-
-          const servicesOptions = new Map([
-            [
-              'prx:give',
-              {
-                icon: 'heart',
-                color: 'secondary'
-              }
-            ]
-          ]);
-          const options = servicesOptions.get(service);
-
-          return {
-            ...options,
-            ...other,
-            service
-          };
-        })
-        .map(
-          ({ color, icon, name, url, key, attributes, itemLinkClass = '' }) => (
-            <Fragment key={key}>
-              <Button
-                sx={{ display: { xs: 'none', sm: 'inline-flex' } }}
-                component="a"
-                href={isLocalUrl(url.href) ? url.pathname || '/' : url.href}
-                onClick={handleButtonClick(url)}
-                variant={
-                  /\bbtn-(text|link)\b/.test(itemLinkClass)
-                    ? 'text'
-                    : 'contained'
-                }
-                color={(color as ButtonColors) || 'primary'}
-                disableRipple
-                disableElevation
-                {...(icon && { startIcon: renderIcon(icon) })}
-                {...attributes}
-              >
-                {name}
-              </Button>
-              {icon ? (
-                <IconButton
-                  sx={{ display: { sm: 'none', xs: 'inline-flex' } }}
-                  component="a"
-                  href={url.href}
-                  onClick={handleButtonClick(url)}
-                  aria-label={name}
-                  color={(color as IconButtonColors) || 'default'}
-                  size="small"
-                  disableRipple
-                  {...attributes}
-                >
-                  {renderIcon(icon)}
-                </IconButton>
-              ) : (
-                <Button
-                  sx={{ display: { sm: 'none', xs: 'inline-flex' } }}
-                  component="a"
-                  href={url.href}
-                  onClick={handleButtonClick(url)}
-                  variant="text"
-                  color={(color as ButtonColors) || 'primary'}
-                  size="small"
-                  disableRipple
-                  disableElevation
-                  {...attributes}
-                  {...(icon && {
-                    startIcon: renderIcon(icon)
-                  })}
-                >
-                  {name}
-                </Button>
-              )}
-            </Fragment>
-          )
-        )}
+      {menuItems.map(renderMenuItem)}
     </ThemeProvider>
-  ) : null;
+  );
 };
